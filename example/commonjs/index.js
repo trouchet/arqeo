1712c4f6a3969888fa717cf5cc425915ef8d59e1
Bruno Peixoto <brunolnetto@gmail.com>
2023-02-18 12:06:58 -0300

    feat/ examples with experiments/print/report utilitary functions

--- example/commonjs/index.js
+++ example/commonjs/index.js
@@ -1,4 +1,47 @@
-const arqeo = require("arqeo");
+const aqo = require("arqeo");
 const _ = require("lodash");
 
-console.log(arqeo.is([1,2,3], _.isNumber))
+const { is, are, has, catalog, apply } = aqo;
+
+/**
+ * 
+ **/
+const print = (question, answer) => {
+    console.log(`${question} ${answer}`)
+} 
+
+const experiment = (candidate, artifactMap, artifactMapDescription, isCallback, isDescription) => {
+    const candidateStr = JSON.stringify(candidate);
+
+    const question = `${artifactMapDescription} candidate ${candidateStr} ${isDescription}?`;
+    const answer = JSON.stringify(artifactMap(candidate, isCallback));
+
+    return {
+      question: question, 
+      answer: answer 
+    };
+}
+
+const report = (candidate, artifactMap, artifactMapDescription, isCallback, isDescription) => {
+    let quest;
+    
+    quest = experiment(candidate, artifactMap, artifactMapDescription, isCallback, isDescription);
+    print(quest.question, quest.answer);
+}
+
+report([1,2,3], is, "Is", _.isNumber, "a number artifact");
+report([1,2,"3"], is, "Is", _.isNumber, "a number artifact");
+
+report([1, [1,2,3]], are, "Are", _.isNumber, "a number collection");
+report([1, [1,2,"3"]], are, "Are", _.isNumber, "a number collection");
+
+report([1, [1,2,3]], has, "Has", _.isString, "a string item");
+report([1, [1,2,"3"]], has, "Has", _.isString, "a string item");
+
+report([1, [1,2,3]], catalog, "Catalog", _.isString, "as string items");
+report([1, [1,2,3]], catalog, "Catalog", _.isNumber, "as number items");
+
+const doubleMap = (num) => 2*num;
+const applyMap = (candidate, isArtifactCallback) => apply(candidate, isArtifactCallback, doubleMap);
+
+report([1,2,3], applyMap, "Apply", _.isNumber, "on number artifact");
